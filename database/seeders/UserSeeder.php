dcf068e64eca576761e9d9fcc5791dee507cbc0b
ArpineMuradyan <arpine.muradyan.2001@gmail.com>
2022-05-02 12:06:41 +0400

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => Hash::make('password'),
+                'birthday' => Carbon::today()->subYear(rand(0, 90))->subMonth(rand(0, 12))->subDays(rand(0, 30))
+            ]);
+        }
+    }
+}
